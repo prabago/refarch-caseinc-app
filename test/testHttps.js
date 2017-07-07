cdc458358b5eaa7151c3e6a4889e10621df07583
Jerome Boyer <boyerje@us.ibm.com>
2017-07-07 06:54:31 -0700

    work on certificate

--- test/testHttps.js
+++ test/testHttps.js
@@ -1,67 +1,50 @@
+/**
+This code explains how to perform a HTTP GET over a TLS connection with mutual authentication.
+The example is for access to a /login path exposed on API Connect gateway.
+*/
 
-var https = require('https');
+//var https = require('https');
 var querystring = require('querystring');
 var request = require('request').defaults({strictSSL: false});
 var fs = require('fs');
 var path = require('path');
-var caCert = fs.readFileSync(path.resolve(__dirname, '../ssl/sg.pem'));
+var clientCert = fs.readFileSync(path.resolve(__dirname, '../ssl/qsn47KM8iTa_O495D_destCert.pem'));
+var clientKey = fs.readFileSync(path.resolve(__dirname, '../ssl/qsn47KM8iTa_O495D_destKey.pem'));
+var caCerts =[];
+caCerts.push(fs.readFileSync(path.resolve(__dirname, '../ssl/DigiCertCA2.pem')));
+caCerts.push(fs.readFileSync(path.resolve(__dirname, '../ssl/secureGatewayCert.pem')));
+caCerts.push(fs.readFileSync(path.resolve(__dirname, '../ssl/qsn47KM8iTa_clientCert.pem')));
 
 //var user = { username: 'case@csplab.local',password:'case01'}
 var user = { username: 'boyerje',password:'case01'}
 
-var options={
-  host:'cap-sg-prd-5.integration.ibmcloud.com',
-  port: 16582,
-  path:'/csplab/sb/sample-inventory-api/login',
-  agentOptions:{
-    ca: [caCert]
-  },
-  headers: {
-    'X-IBM-Client-Id': "5d2a6edb-793d-4193-b9b0-0a087ea6c123",
-    'accept': 'application/json',
-    'content-type': 'application/json'
-  },
-  qs:querystring.stringify(user)
-}
 
 var builtUrl='https://cap-sg-prd-5.integration.ibmcloud.com:16582/csplab/sb/sample-inventory-api/login?'+querystring.stringify(user);
 // generate xml payload
 //var builtUrl='https://cap-sg-prd-5.integration.ibmcloud.com:16582/csplab/sb/sample-inventory-api/login';
 // fail timeout then 500
-var builtUrl='https://cap-sg-prd-5.integration.ibmcloud.com:16582/csplab/sb/sample-inventory-api/login?username=case%40usibm&password=case01';
+//var builtUrl='https://cap-sg-prd-5.integration.ibmcloud.com:16582/csplab/sb/sample-inventory-api/login?username=case%40usibm&password=case01';
 // work!
-var builtUrl='https://172.16.50.8/csplab/sb/sample-inventory-api/login?username=case%40usibm&password=case01';
+//var builtUrl='https://172.16.50.8/csplab/sb/sample-inventory-api/login?username=case%40usibm&password=case01';
 
+var options={
+  url: builtUrl,
+ key: clientKey,
+ cert: clientCert,
+ ca: caCerts,
+ passphrase: 'casecret',
+ rejectUnauthorized: true,
+  headers: {
+    'X-IBM-Client-Id': "5d2a6edb-793d-4193-b9b0-0a087ea6c123",
+    'accept': 'application/json'
+  }
+}
 console.log("Call with url:"+builtUrl);
-request({
-    method: 'GET',
-    url: builtUrl,
-    headers: {
-      'X-IBM-Client-Id': '5d2a6edb-793d-4193-b9b0-0a087ea6c123'
-    },
-   agentOptions:{
-     ca: caCert
-   },
-  }, function (error, response, body) {
-      console.log(body);
+request.get(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-         console.log( body.data));
+         console.log( body);
       }
       if (error) {
         console.log("Error "+error);
-        console.log( error.data));
       }
 });
-
-/*
-https.get(options, function(res) {
-  console.log(res.body);
-   res.on("data", function(d) {
-     console.log( JSON.parse(d.toString()));
-   });
-
-   res.on("error", function(e) {
-     console.log( JSON.parse(e.toString()));
-   });
- });
- */
